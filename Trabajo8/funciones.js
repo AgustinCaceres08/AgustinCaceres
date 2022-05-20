c3a210c04c3f062d97241969e8e6bbf42b56459c
Agustin Caceres <caceres.ls@hotmail.com>
2022-05-20 19:17:19 -0300

    Modifico Trabajo8

--- Trabajo8/funciones.js
+++ Trabajo8/funciones.js
@@ -19,9 +19,23 @@ function mostrar(){
             Foto: <br>
              <img src="${data.results[0].picture.large}">
         ` 
+        var lat = data.results[0].location.coordinates.latitude;
+        var lon = data.results[0].location.coordinates.longitude;
+
         console.log(data.results[0])
+
     if (data.results[0].dob.age >= 20 && data.results[0].dob.age <=40 && data.results[0].gender == 'female') {
         document.getElementById('cont').innerHTML=html
+
+        var map = L.map('map').setView([lat, lon], 13);
+
+        L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18
+        }).addTo(map);
+    
+        L.control.scale().addTo(map);
+        L.marker([lat, lon], {draggable: true}).addTo(map);
     }
     else {
         document.getElementById('cont').innerHTML='Usuario no encontrado'
