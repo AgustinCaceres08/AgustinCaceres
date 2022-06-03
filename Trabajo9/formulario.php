4036a430e7431dbc82e956cfdff2e4b9f0abb73b
Agustin Caceres <caceres.ls@hotmail.com>
2022-06-02 21:16:18 -0300

    Modifico Trabajo9

--- Trabajo9/formulario.php
+++ Trabajo9/formulario.php
@@ -4,4 +4,16 @@
 
     $aux = $_POST["nombre"];
     echo $aux;
+
+    $aux2 = $_POST["apellido"];
+    echo $aux2;
+
+    $aux3 = $_POST["genero"];
+    echo $aux3;
+
+    if (isset($_POST["color"])) {
+        foreach ($_POST["color"] as $key => $value) {
+            
+        }
+    }
 ?>
