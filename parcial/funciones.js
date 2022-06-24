a45e6c495b93a768d3935547508e8f76c1c65c12
Agustin Caceres <caceres.ls@hotmail.com>
2022-06-24 19:18:51 -0300

    Agrego primer parcial

---
+++ parcial/funciones.js
@@ -0,0 +1,72 @@
+const SOCIO1 = {
+    apellido: 'Caceres',
+    nombre: 'Agustin',
+    edad: 21,
+    facturas: 1,
+    dni: 1
+};
+
+const SOCIO2 ={
+    apellido: 'Perez',
+    nombre: 'Enzo',
+    edad: 52,
+    facturas: 6,
+    dni: 2
+};
+
+const SOCIO3 = {
+    apellido: 'Fernandez',
+    nombre:'Maria',
+    edad: 39,
+    facturas: 3,
+    dni: 3
+};
+
+const SOCIO4 = {
+    apellido: 'Gonzalez',
+    nombre:'Valentina',
+    edad: 19,
+    facturas: 20,
+    dni: 4
+};
+
+const SOCIO5 = {
+    apellido:'Ramos',
+    nombre: 'Juan',
+    edad: 14,
+    facturas: 4,
+    dni: 5
+};
+
+let socios = [SOCIO1,SOCIO2,SOCIO3,SOCIO4,SOCIO5];
+
+function enviar() {
+    let dni = document.getElementById("dni").value
+    let resultado
+
+    socios.forEach(element => {
+        if (element.dni == dni) {
+         resultado = element 
+        }
+     });
+    
+    if (resultado==undefined) {
+        document.getElementById("div").style.background = "red";
+        document.getElementById("div").innerHTML = "No existe";
+    }else{
+        if(resultado.facturas<=3){
+            document.getElementById("div").style.background = "green"
+            document.getElementById("div").innerHTML = "Nombre: "+resultado.nombre+`<br>`+"Apellido: "+resultado.apellido+"<br>"+"Facturas: "+resultado.facturas;
+        }
+    
+        if(resultado.facturas>3 && resultado.facturas<6){
+            document.getElementById("div").style.background = "yellow"
+            document.getElementById("div").innerHTML = "Nombre: "+resultado.nombre+`<br>`+"Apellido: "+resultado.apellido+"<br>"+"Facturas: "+resultado.facturas;;
+        }
+    
+        if(resultado.facturas>=6){
+            document.getElementById("div").style.background = "red"
+            document.getElementById("div").innerHTML = "Nombre: "+resultado.nombre+`<br>`+"Apellido: "+resultado.apellido+"<br>"+"Facturas: "+resultado.facturas;;
+        }
+    }
+}
